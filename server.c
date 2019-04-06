f9e18c660fc309d1602f1d5ab87ba707148b91ea
TheGreatS <spiencewelch@gmail.com>
2019-04-05 21:50:32 -0700

    updated server

--- server.c
+++ server.c
@@ -1014,12 +1014,13 @@ int normal(int sockfd, int numbytes)
   char *pub_key = "public key\0";
   if (send(sockfd, pub_key, 12, 0) == -1)
     perror("send");
+  printf("sent key\n");
   char pub_key_in[500];
   if ((numbytes = recv(sockfd, pub_key_in, 500, 0)) == -1) {
     perror("recv");
     exit(1);
   }
-  printf("got key");
+  printf("got key\n");
   if (send(sockfd, "_", 1, 0) == -1)
     perror("send");
   char type[4];
