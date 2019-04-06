63bbe660331e4002732c8b10335bd46190975ef0
TheGreatS <spiencewelch@gmail.com>
2019-04-05 23:34:35 -0700

    updated server

--- server.c
+++ server.c
@@ -931,24 +931,6 @@ void getTransactionList(int sockfd,int numbytes)
       perror("recv");
       exit(1);
     }
-    if (send(sockfd, "d", 1, 0) == -1) // stop the update
-      perror("send");
-    if ((numbytes = recv(sockfd, buffer, 1, 0)) == -1) {
-      perror("recv");
-      exit(1);
-    }
-    if (send(sockfd, "e", 1, 0) == -1) // stop the update
-      perror("send");
-    if ((numbytes = recv(sockfd, buffer, 1, 0)) == -1) {
-      perror("recv");
-      exit(1);
-    }
-    if (send(sockfd, "f", 1, 0) == -1) // stop the update
-      perror("send");
-    if ((numbytes = recv(sockfd, buffer, 1, 0)) == -1) {
-      perror("recv");
-      exit(1);
-    }
   }
   if (send(sockfd, "s", 1, 0) == -1) // stop the update
     perror("send");
